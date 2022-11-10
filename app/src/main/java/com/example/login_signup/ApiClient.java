567f052c01f88e412376467570b5e8d9e2b348b3
roseLea <lealynfroilan00@gmail.com>
2022-11-10 20:35:45 +0800

    new update

--- app/src/main/java/com/example/login_signup/ApiClient.java
+++ app/src/main/java/com/example/login_signup/ApiClient.java
@@ -4,7 +4,7 @@ import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
-import retrofit2.converter.scalars.ScalarsConverterFactory;
+
 
 public class ApiClient {
     private static ApiClient instance =null;
@@ -17,7 +17,7 @@ public class ApiClient {
 
 
         Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("http://192.168.1.6:8000/api/")
+                .baseUrl("http://192.168.254.105:8000/api/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .client(okHttpClient)
                 .build();
