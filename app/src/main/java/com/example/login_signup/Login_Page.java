486faada933fc5a96aa20fab7dbef5102064bea0
roseLea <lealynfroilan00@gmail.com>
2022-11-21 16:23:59 +0800

    UI UPDATE

--- app/src/main/java/com/example/login_signup/Login_Page.java
+++ app/src/main/java/com/example/login_signup/Login_Page.java
@@ -34,6 +34,7 @@ public class Login_Page extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.login_page);
         queue = Volley.newRequestQueue(this);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
